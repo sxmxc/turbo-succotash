a925fc8bb174dbd642e1b27ff26a9db93dfddf02
devadmin <steven.mcneely@live.com>
2026-09-10 04:00:30 +0000

    build tools fix

--- scripts/compose.mjs
+++ scripts/compose.mjs
@@ -35,8 +35,6 @@ if (process.argv[1]?.endsWith("/compose.mjs")) {
         "--wait",
         "--wait-timeout",
         "120",
-        "--scale",
-        "migrate=0",
       ],
       env,
     );
